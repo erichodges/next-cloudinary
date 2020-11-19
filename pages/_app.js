3457e32692ea1575709cc3c72a734c60f7799713
Eric Hodges <ehodges2004@gmail.com>
2020-11-19 14:36:00 -0800

    Setup: Initial commit - fresh Nextjs site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
+
+// NOTES
+// The Component prop is the active page, so whenever you navigate between routes,
+// Component will change to the new page. Therefore, any props you send to Component
+// will be received by the page.
