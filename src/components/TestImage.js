bd827303fe5e9d18170b272be00adf4351865dd0
Eric Hodges <ehodges2004@gmail.com>
2020-11-20 14:58:19 -0800

    Setup: Moved use-cloudinary func to a component

--- src/components/TestImage.js
+++ src/components/TestImage.js
@@ -1,18 +1,30 @@
+import React from 'react';
 import { useImage } from 'use-cloudinary';
 
 const TestImage = () => {
-  const config = {
-    delivery: {
-      publicId: 'your-public-id'
-    },
-    transformation: {
-      // add your transformations here
-    }
+  const { generateImageUrl } = useImage(process.env.CLOUDINARY_CLOUD_NAME);
+
+  const images = [
+    'gatsby-cloudinary/evora-gt',
+    'gatsby-cloudinary/evora-gt-side',
+    'gatsby-cloudinary/evija-side'
+  ];
+
+  const renderMultipleImages = () => {
+    return images.map((img) => {
+      const config = {
+        delivery: {
+          publicId: img
+        },
+        transformation: {
+          // whatever
+        }
+      };
+      return <img key={img} src={generateImageUrl(config)} />;
+    });
   };
-
-  const { generateImageUrl } = useImage('your-cloud-name');
-
-  <img src={generateImageUrl(config)} />;
+  // console.log(process.env.CLOUDINARY_CLOUD_NAME);
+  return <div>{renderMultipleImages()}</div>;
 };
 
 export default TestImage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import { useImage } from 'use-cloudinary';
+// import { useImage } from 'use-cloudinary';
+import TestImage from '../components/TestImage';
 
 export default function Home() {
-  const { generateImageUrl } = useImage(process.env.CLOUDINARY_CLOUD_NAME);
-  console.log(process.env.CLOUDINARY_CLOUD_NAME);
-  const images = [
-    'gatsby-cloudinary/evora-gt',
-    'gatsby-cloudinary/evora-gt-side',
-    'gatsby-cloudinary/evija-side'
-  ];
-
-  const renderMultipleImages = () => {
-    return images.map((img) => {
-      const config = {
-        delivery: {
-          publicId: img
-        },
-        transformation: {
-          // whatever
-          // height: 0.3
-        }
-      };
-      return <img key={img} src={generateImageUrl(config)} />;
-    });
-  };
-  // console.log(process.env.CLOUDINARY_CLOUD_NAME);
-  return <div>{renderMultipleImages()}</div>;
+  return (
+    <div>
+      <TestImage />
+    </div>
+  );
 }
