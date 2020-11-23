f389ac3320def4380fedd39c604cadb4bd80d003
Eric Hodges <ehodges2004@gmail.com>
2020-11-23 13:50:36 -0800

    Setup: Testing ImageComponent settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import React from 'react';
 
 const ImageComponent = ({
   src = 'evora-gt-side',
-  layout = 'fill',
+  width = 2400,
+  height = 1200,
   loading = 'lazy'
 }) => {
   const hostUrl = `https://res.cloudinary.com/dn3baky3u/image/upload/w_auto,q_auto,f_auto,dpr_auto/v1580246844/gatsby-cloudinary`;
@@ -16,9 +17,11 @@ const ImageComponent = ({
       className='object-center object-cover w-full h-full'
       src={`${hostUrl}/${src}`}
       alt='Picture of the author'
-      layout={layout}
+      layout='responsive'
       quality={10}
       loading={loading}
+      width={width}
+      height={height}
     />
   );
 };
